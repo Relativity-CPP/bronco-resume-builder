3158302e69bd5091a022e2a3e8ae5228622b35fd
Aaron Lewis <aaron11125@gmail.com>
2019-09-25 22:09:33 -0700

    Added the materials library to the project and added 3 components which can be accessed via HTTP

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './component/edit/edit.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'create', component: CreateComponent},
+{ path: 'edit/:id', component:EditComponent},
+{ path: '', redirectTo: 'list', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+  	RouterModule.forRoot(routes)
+  	],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MatToolbarModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './component/edit/edit.component';
+const routes: Routes = [
+{ path: 'create', component: CreateComponent},
+{ path: 'edit/:id', component: EditComponent},
+{ path: 'list', component: ListComponent},
+{ path: '', redirectTo: 'list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
