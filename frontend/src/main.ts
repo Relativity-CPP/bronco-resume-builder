984c8e477c8437c13dd03feaef5434f39d371d2c
Gary M <50461559+aSilverSoul@users.noreply.github.com>
2019-10-02 18:13:21 -0700

    added primeng

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';
+
 if (environment.production) {
   enableProdMode();
 }
