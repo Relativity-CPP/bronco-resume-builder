00ce9a9ffff196819cab8a8ec123854cd7245ef3
Aaron Lewis <aaron11125@gmail.com>
2019-10-27 18:29:26 -0700

    Fixed missing forward slash in express app file which prevented POST requests. Also removed redundant folder frontend.

--- bronco-resume-app/backend/app.js
+++ bronco-resume-app/backend/app.js
@@ -1,32 +1,54 @@
+// import express, bodyParser, mongoose
 const express = require('express');
 const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+
+const ContactInfo = require('./models/contact-info');
 
 const app = express();
 
+// connect to mongoDB
+mongoose.connect('mongodb+srv://aaronL:doXbB996gyUniohb@cluster0-kxg8w.mongodb.net/bronco-resume-info?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+    .catch(() => {
+      console.log('Connection failed!');
+    });
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
 });
 
-app.post('api/contact-info', (req, res, next) => {
-  const contactInfo = req.body;
+app.post('/api/contact-info', (req, res, next) => {
+  const contactInfo = new ContactInfo({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    homeAddress: req.body.homeAddress,
+    phoneNumber: req.body.phoneNumber,
+    emailAddress: req.body.emailAddress,
+    socialMediaLink: req.body.socialMediaLink
+  });
+  contactInfo.save();
   console.log(contactInfo);
   res.status(201).json({
     message: 'Contact info added successfully.',
   });
 });
 
-app.use('/api/contact-info', (req, res, next) => {
+app.get('/api/contact-info', (req, res, next) => {
   const contactInfo = {
     id: 'test123',
     firstName: 'First Name',
