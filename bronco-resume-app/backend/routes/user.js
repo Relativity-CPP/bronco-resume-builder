7824bcc2e8851755f2b7c28efd7d6a336d92b264
Alexander Wong <41557633+wongalex349@users.noreply.github.com>
2019-11-02 20:20:47 -0700

    pushed the login page

---
+++ bronco-resume-app/backend/routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../model/user");
+
+const router = express.Router();
+
+router.post("/signup",(req,res,next) => {
+  bcrypt.hash(req.password,10,)
+    .then(hash => {
+      const user = new User({
+        email:req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'User created!',
+            result: result
+          });
+        })
+        .catch(err =>{
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
+});
+
+module.exports = router;
---
+++ bronco-resume-app/src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  isLoading = false;
+  onLogin(form: NgForm) {
+    console.log(form.value);
+  }
+}
+
