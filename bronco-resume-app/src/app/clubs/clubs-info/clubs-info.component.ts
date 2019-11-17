36bc17dc3b122c610350e9620287dbea69154566
Alejandro Millan <alejandro_millan@ymail.com>
2019-11-16 23:55:15 -0800

    Added the links to the clubs webpages

--- bronco-resume-app/src/app/clubs/clubs-info/clubs-info.component.ts
+++ bronco-resume-app/src/app/clubs/clubs-info/clubs-info.component.ts
@@ -3,42 +3,49 @@ import { Component } from "@angular/core";
 export interface PeriodicElement {
   name: string;
   description: string;
+  weblink: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {
     name: "CSS",
     description:
-      "Encourage the development of software engineering skills in students"
+      "Encourage the development of software engineering skills in students",
+    weblink: "https://cppcss.club"
   },
   // tslint:disable-next-line:max-line-length
   {
     name: "She Code",
     description:
-      "Work to promote and support the growing community of women, including allies, in computer science and technology"
+      "Work to promoteand support the growing community of women, including allies, in computer science and technology",
+    weblink: "https://www.cpp.edu/~shecodes/"
   },
   // tslint:disable-next-line:max-line-length
   {
     name: "Software Engineering Association",
     description:
-      "Teaches and encourages the professional skills needed to be a Software Engineer"
+      "Teaches and encourages the professional skills needed to be a Software Engineer",
+    weblink: "https://www.cppsea.com"
   },
   // tslint:disable-next-line:max-line-length
   {
     name: "Swift",
     description:
-      "Dynamic landscape of information security, cyber security, and information technology"
+      "Dynamic landscape of information security, cyber security, and information technology",
+    weblink: "https://www.calpolyswift.org"
   },
   {
-    name: "Game Dev",
+    name: "Game Design and Development",
     description:
-      "Promote and educate students about the world of game design and development"
+      "Promote and educate students about the world of game design and development",
+    weblink: "https://www.cppgamedev.com"
   },
   // tslint:disable-next-line:max-line-length
   {
     name: "Fast",
     description:
-      "Study Digital Forensics, Cyber Crime, and Cyber Security by working with government agencies, and private firms"
+      "Study Digital Forensics, Cyber Crime, and Cyber Security by working with government agencies, and private firms",
+    weblink: "https://www.cppfast.org"
   }
 ];
 
